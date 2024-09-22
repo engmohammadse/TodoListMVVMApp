0d59ef0b048f6b0930b70bef9a1339f545c51734
muhammed <mohammadalaaraji@gmail.com>
2024-09-23 01:18:34 +0300

    Add a ViewModel with @EnvironmentObject in SwiftUI | Todo List #3 - add .onDelete(perform: deleteItem)             .onMove(perform: moveItem)
    T: 5:03

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var item: [ItemModel] = [
+    @State var items: [ItemModel] = [
         
         ItemModel(title: "This is the First Item!", isCompleted: false),
         ItemModel(title: "This is the Second Item!", isCompleted: true),
@@ -21,10 +21,12 @@ struct ListView: View {
     var body: some View {
         List{
           
-            ForEach(item) { item in
+            ForEach(items) { item in
                
                 ListRowView(item: item)
             }
+            .onDelete(perform: deleteItem)
+            .onMove(perform: moveItem)
             
         }
         .listStyle(PlainListStyle())
@@ -34,37 +36,16 @@ struct ListView: View {
             leading: EditButton(),
             
             trailing:
-                NavigationLink("Add", destination: AddView())
-        
-        )
-        
-        
-        //new
-//        .toolbar {
-//            
-//            
-//            ToolbarItem(placement: .topBarLeading) {
-//                Button(action: {
-//                   
-//                   
-//                }) {
-//                    NavigationLink("Add", destination: Text("Destination"))
-//                    
-//                }
-//            }
-//            //
-//            
-//            
-//            ToolbarItem(placement: .topBarTrailing) {
-//                Button(action: {
-//                    print("Trailing action")
-//                }) {
-//                    Text("Add")
-//                }
-//                
-//            }
-//            
-//        }// toolbar
+                NavigationLink("Add", destination: AddView()))
+      
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem(from: IndexSet, to: Int){
+        items.move(fromOffsets: from, toOffset: to)
     }
 }
 
@@ -74,3 +55,34 @@ struct ListView: View {
     }
 }
 
+
+
+
+
+//old navigationBarItems
+//new
+//        .toolbar {
+//
+//
+//            ToolbarItem(placement: .topBarLeading) {
+//                Button(action: {
+//
+//
+//                }) {
+//                    NavigationLink("Add", destination: Text("Destination"))
+//
+//                }
+//            }
+//            //
+//
+//
+//            ToolbarItem(placement: .topBarTrailing) {
+//                Button(action: {
+//                    print("Trailing action")
+//                }) {
+//                    Text("Add")
+//                }
+//
+//            }
+//
+//        }// toolbar
